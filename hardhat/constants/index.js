4fd33882fd57a625d9fac95d6e7522fb37276864
Richy Chen <richy@Richys-MacBook.local>
2022-02-11 10:28:26 -0500

    completed dao tutorial

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x5Cb27c76089a38eb9B11fB76AC5A11d0E779f50C";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
